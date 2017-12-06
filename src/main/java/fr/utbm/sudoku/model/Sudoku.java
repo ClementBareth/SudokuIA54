a7409e7ee522033f974cac7aa728ee8aa5a25513
Clement Bareth <clement.bareth@gmail.com>
2017-12-06 13:28:16 +0100

    Init agent 1/2

--- src/main/java/fr/utbm/sudoku/model/Sudoku.java
+++ src/main/java/fr/utbm/sudoku/model/Sudoku.java
@@ -1,25 +1,28 @@
 package fr.utbm.sudoku.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.dizitart.no2.objects.Id;
 
 public class Sudoku {
 
-	private int[][] matrix = new int[9][9];
+	private Integer[][] matrix = new Integer[9][9];
 	private Difficulty difficulty;
-	
+
 	@Id
 	private String name;
-	
+
 	public int getValue(int x, int y) {
-		return matrix[x][y];
+		return this.matrix[x][y];
 	}
-	
+
 	public void setValue(int x, int y, int value) {
-		matrix[x][y] = value;
+		this.matrix[x][y] = value;
 	}
 
 	public Difficulty getDifficulty() {
-		return difficulty;
+		return this.difficulty;
 	}
 
 	public void setDifficulty(Difficulty difficulty) {
@@ -27,12 +30,36 @@ public class Sudoku {
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
+	public List<Integer> getRow(int rowNum) {
+		ArrayList<Integer> row = new ArrayList<>();
+		for (int i = 0; i < 9; i++) {
+			row.add(this.matrix[i][rowNum]);
+		}
+		return row;
+	}
+
+	public List<Integer> getColumn(int colNum) {
+		ArrayList<Integer> col = new ArrayList<>();
+		for (int i = 0; i < 9; i++) {
+			col.add(this.matrix[colNum][i]);
+		}
+		return col;
+	}
+
+	public List<Integer> getRegion(int rowStart, int rowEnd, int colStart, int colEnd) {
+		ArrayList<Integer> region = new ArrayList<>();
+		for (int i = rowStart; i < rowEnd; i++) {
+			for (int j = colStart; j < colEnd; j++) {
+				region.add(this.matrix[i][j]);
+			}
+		}
+		return region;
+	}
 }
